a70e4cb12018f800b463c986a4d6d5125e6819fc
tatsuya-akitsu-cfo <tatsuya-akitsu@c-fo.com>
2023-11-13 17:54:44 +0900

    seed調整

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,25 +1,5 @@
 import { CharacterAttributes, CharacterType } from "@prisma/client"
 
-interface CharactersSeed {
-  name: string
-  label: string
-  attributes: CharacterAttributes
-  type: CharacterType
-  totalScore: number
-  boost: 0 | 1 | 2 | 3
-  hasCharacter: boolean
-  userId: number
-}
-
-interface CharacterTagSeed {
-  name: string;
-  isActivation: boolean;
-  effect: Array<{
-    level: number;
-    effect: string;
-  }>;
-}
-
 interface CharacterSkill {
   name: string;
   effects: Array<{
@@ -45,6 +25,49 @@ interface Characters {
   boost: 0 | 1 | 2 | 3;
   attributes: CharacterAttributes;
   type: CharacterType;
+  status: {
+    level: number;
+    totalScore: number;
+    strength: number;
+    attack: number;
+    defense: number;
+    critical: number;
+    charaBoost: number;
+  };
+  skill: {
+    skill1: CharacterSkill | Array<CharacterSkill>;
+    skill2: CharacterSkill | Array<CharacterSkill>;
+    teamBoost: {
+      name: string;
+      effect: string;
+    };
+  };
+  property: CharacterProperty;
+  hasCharacter: boolean;
+  userId: number;
+}
+
+interface CharactersSeed {
+  name: string;
+  label: string;
+  attributes: CharacterAttributes;
+  type: CharacterType;
+  totalScore: number;
+  boost: 0 | 1 | 2 | 3;
+  hasCharacter: boolean;
+  userId: number;
+}
+
+interface CharacterTagSeed {
+  name: string;
+  isActivation: boolean;
+  effect: Array<{
+    level: number;
+    effect: string;
+  }>;
+}
+
+interface CharacterDetailSeed {
   status: {
     level: number;
     totalScore: number;
